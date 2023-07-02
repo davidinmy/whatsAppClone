a970a763ebca63aac32ec7cde208781b081ebe1d
David He <heliushanchuan@gmail.com>
2023-07-02 21:14:05 +0800

    implement chat screen ui

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,16 +1,67 @@
-import React from "react";
-import { View, Text, StyleSheet, Button, ImageBackground } from "react-native";
+import React, { useCallback, useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  ImageBackground,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Feather } from "@expo/vector-icons";
 
+import colors from "../constants/colors";
 import backgroundImage from "../assets/images/ON9NJN0.jpg";
 
 const ChatScreen = (props) => {
+  const [messageText, setMessageText] = useState("");
+
+  const sendMessage = useCallback(() => {
+    setMessageText("");
+  }, [messageText]);
+
   return (
-    <View style={styles.container}>
+    <SafeAreaView edges={["right", "left", "bottom"]} style={styles.container}>
       <ImageBackground
         source={backgroundImage}
         style={styles.backgroundImage}
       ></ImageBackground>
-    </View>
+
+      <View style={styles.inputContainer}>
+        <TouchableOpacity
+          style={styles.mediaButton}
+          onPress={() => console.log("pressed")}
+        >
+          <Feather name="plus" size={24} color={colors.blue} />
+        </TouchableOpacity>
+
+        <TextInput
+          style={styles.textbox}
+          value={messageText}
+          onChangeText={(text) => setMessageText(text)}
+          onSubmitEditing={sendMessage}
+        />
+
+        {messageText === "" && (
+          <TouchableOpacity
+            style={styles.mediaButton}
+            onPress={() => console.log("pressed")}
+          >
+            <Feather name="camera" size={24} color={colors.blue} />
+          </TouchableOpacity>
+        )}
+
+        {messageText !== "" && (
+          <TouchableOpacity
+            style={{ ...styles.mediaButton, ...styles.sendButton }}
+            onPress={sendMessage}
+          >
+            <Feather name="send" size={20} color={"white"} />
+          </TouchableOpacity>
+        )}
+      </View>
+    </SafeAreaView>
   );
 };
 
@@ -21,6 +72,31 @@ const styles = StyleSheet.create({
   },
   backgroundImage: {
     flex: 1,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    paddingVertical: 8,
+    paddingHorizontal: 10,
+    height: 50,
+  },
+  textbox: {
+    flex: 1,
+    borderWidth: 1,
+    borderRadius: 50,
+    borderColor: colors.lightGrey,
+    marginHorizontal: 15,
+    paddingHorizontal: 12,
+  },
+  mediaButton: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: 35,
+  },
+  sendButton: {
+    backgroundColor: colors.blue,
+    borderRadius: 50,
+    padding: 8,
+    width: 35,
   },
 });
 
