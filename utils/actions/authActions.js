665d291e945e591635872487c1c436152b13b66e
David He <heliushanchuan@gmail.com>
2023-07-05 17:36:52 +0800

    sending userdata to the database

--- utils/actions/authActions.js
+++ utils/actions/authActions.js
@@ -1,5 +1,6 @@
 import { getFireBaseApp } from "../firebaseHelper";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getDatabase, ref, child, set } from "firebase/database";
 
 export const signUp = async (firstName, lastName, email, password) => {
   const app = getFireBaseApp();
@@ -7,8 +8,13 @@ export const signUp = async (firstName, lastName, email, password) => {
 
   try {
     const result = await createUserWithEmailAndPassword(auth, email, password);
+    const { uid } = result.user;
+
+    const userData = await createUser(firstName, lastName, email, uid);
+
     console.log(result);
   } catch (error) {
+    console.log(error);
     const errorCode = error.code;
 
     let message = "something went wrong";
@@ -19,4 +25,21 @@ export const signUp = async (firstName, lastName, email, password) => {
 
     throw new Error(message);
   }
+};
+
+const createUser = async (firstName, lastName, email, userId) => {
+  const firstLast = `${firstName} ${lastName}`.toLowerCase();
+  const userData = {
+    firstName,
+    lastName,
+    firstLast,
+    email,
+    userId,
+    signUpDate: new Date().toISOString(),
+  };
+
+  const dbRef = ref(getDatabase());
+  const childRef = child(dbRef, `users/${userId}`);
+  await set(childRef, userData);
+  return userData;
 };
