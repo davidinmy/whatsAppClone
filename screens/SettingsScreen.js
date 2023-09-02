359b75ee098c577490e757a3d9536f2ce85abd81
David He <heliushanchuan@gmail.com>
2023-09-02 20:23:54 +0800

    Adding the input fields

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,12 +1,81 @@
-import React from "react";
+import React, { useCallback, useReducer } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import Input from "../components/Input";
 import PageContainer from "../components/PageContainer";
 import PageTitle from "../components/PageTitle";
+import { validateInput } from "../utils/actions/formActions";
+import { reducer } from "../utils/reducers/formReducer";
+
+import { FontAwesome, Feather } from "@expo/vector-icons";
+
+const initialState = {
+  inputValues: {
+    firstName: "",
+    lastName: "",
+    email: "",
+    about: "",
+  },
+  inputValidities: {
+    firstName: false,
+    lastName: false,
+    email: false,
+    about: false,
+  },
+  formIsValid: false,
+};
 
 const SettingsScreen = (props) => {
+  const [formState, dispatchFormState] = useReducer(reducer, initialState);
+
+  const inputChangeHandler = useCallback(
+    (inputId, inputValue) => {
+      const result = validateInput(inputId, inputValue);
+      dispatchFormState({ inputId, validationResult: result, inputValue });
+    },
+    [dispatchFormState]
+  );
+
   return (
     <PageContainer>
       <PageTitle text="Settings" />
+
+      <Input
+        id="firstName"
+        label="First name"
+        icon="user-o"
+        autoCapitalize="none"
+        iconPack={FontAwesome}
+        onInputChanged={inputChangeHandler}
+        errorText={formState.inputValidities["firstName"]}
+      />
+      <Input
+        id="lastName"
+        label="Last name"
+        icon="user-o"
+        autoCapitalize="none"
+        iconPack={FontAwesome}
+        onInputChanged={inputChangeHandler}
+        errorText={formState.inputValidities["lastName"]}
+      />
+      <Input
+        id="email"
+        label="Email"
+        icon="mail"
+        autoCapitalize="none"
+        keyboardType="email-address"
+        iconPack={Feather}
+        onInputChanged={inputChangeHandler}
+        errorText={formState.inputValidities["email"]}
+      />
+      <Input
+        id="about"
+        label="About"
+        icon="user-o"
+        autoCapitalize="none"
+        iconPack={FontAwesome}
+        onInputChanged={inputChangeHandler}
+        errorText={formState.inputValidities["about"]}
+      />
     </PageContainer>
   );
 };
